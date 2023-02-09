8663ecb5e37565cd22fb7bef71ea2ba356e8442b
Gonza <xixon2002@gmail.com>
2023-02-09 19:26:55 +0100

    removing setupNodeEvents from cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,9 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
-  video: true,
+  video: false,
   e2e: {
-    setupNodeEvents(on, config) {},
+    //setupNodeEvents(on, config) {},
     supportFile: false,
   },
 })
